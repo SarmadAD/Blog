def56382c8e3f4e88bd9858da664d6a5c154f232
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-05-09 12:34:01 +0200

    add view and viewmodel for user management, add grid, load user data to grid

---
+++ Blog/View/UserManagement.razor.cs
@@ -0,0 +1,15 @@
+﻿using Blog.ViewModel;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.View
+{
+    public class UserManagementBase:ComponentBase
+    {
+        [Inject] public UserManagementViewModel ViewModel { get; set; }
+
+        protected override void OnInitialized()
+        {
+            ViewModel.LoadUser();
+        }
+    }
+}
---
+++ Blog/ViewModel/UserManagementViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog.ViewModel
+{
+    public class UserManagementViewModel:BaseViewModel
+    {
+        private IDbContextFactory<BlogContext> dbContextFactory;
+        public IEnumerable<User> UserList { get; set; }
+        public UserManagementViewModel(IDbContextFactory<BlogContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        public void LoadUser()
+        {
+            try
+            {
+                IsLoading = true; 
+                using var ctx = dbContextFactory.CreateDbContext();
+                UserList = ctx.Users.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
