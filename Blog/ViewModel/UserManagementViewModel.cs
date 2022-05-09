26bfc8e186e1ff943d6f7fe70d4f7c2c26fc76b9
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-05-09 13:06:13 +0200

    add empty methods for CRUD operations for user, update rows of grid

--- Blog/ViewModel/UserManagementViewModel.cs
+++ Blog/ViewModel/UserManagementViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using Blog.Models;
 using Microsoft.EntityFrameworkCore;
+using Radzen.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Blog.ViewModel
 {
-    public class UserManagementViewModel:BaseViewModel
+    public class UserManagementViewModel : BaseViewModel
     {
         private IDbContextFactory<BlogContext> dbContextFactory;
         public IEnumerable<User> UserList { get; set; }
+
+        public RadzenDataGrid<User> UserGrid { get; set; }
         public UserManagementViewModel(IDbContextFactory<BlogContext> dbContextFactory)
         {
             this.dbContextFactory = dbContextFactory;
@@ -19,7 +22,7 @@ namespace Blog.ViewModel
         {
             try
             {
-                IsLoading = true; 
+                IsLoading = true;
                 using var ctx = dbContextFactory.CreateDbContext();
                 UserList = ctx.Users.ToList();
             }
@@ -30,6 +33,31 @@ namespace Blog.ViewModel
             {
                 IsLoading = false;
             }
+        }
+        public async void SaveUser(User updatedUser)
+        {
+            await UserGrid.UpdateRow(updatedUser);
+        }
+
+        public void CreateUser(User createUser) { }
+        public void DeleteUser(User deletedUser) { }
+        public async void EditUser(User updatedUser)
+        {
+            await UserGrid.EditRow(updatedUser);
+        }
+        public void CancelEdit(User cancelUser)
+        {
+            UserGrid.CancelEditRow(cancelUser);
+        }
+
+        public void OnEditUser()
+        {
+            //Context.Save usw
+        }
+
+        public void OnCreateUser()
+        {
+            //Context.Save usw
         }
     }
 }
