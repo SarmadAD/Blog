05c9ea5c8a13b0348b832e5c91fe6b0a1800b9e9
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-06-01 09:40:19 +0200

    add delete user, add update user

--- Blog/ViewModel/UserManagementViewModel.cs
+++ Blog/ViewModel/UserManagementViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Blog.Models;
+﻿using Blog.Classes.ObjClasses;
+using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 using Radzen.Blazor;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Blog.ViewModel
@@ -24,7 +26,7 @@ namespace Blog.ViewModel
             {
                 IsLoading = true;
                 using var ctx = dbContextFactory.CreateDbContext();
-                UserList = ctx.Users.ToList();
+                UserList = ctx.Users.Where(x=>x.State==(int)EnumClass.State.Active).ToList();
             }
             catch (Exception ex)
             {
@@ -40,19 +42,38 @@ namespace Blog.ViewModel
         }
 
         public void CreateUser(User createUser) { }
-        public void DeleteUser(User deletedUser) { }
+        public void DeleteUser(User deletedUser)
+        {
+            using var ctx = dbContextFactory.CreateDbContext();
+            ctx.Users.FirstOrDefault(x => x.Id == deletedUser.Id).State = (int)EnumClass.State.Deleted;
+            ctx.SaveChanges();
+        }
         public async void EditUser(User updatedUser)
         {
             await UserGrid.EditRow(updatedUser);
+            //Debug.WriteLine(ConvertObject.ConvertObjectToJson(updatedUser));
         }
-        public void CancelEdit(User cancelUser)
-        {
-            UserGrid.CancelEditRow(cancelUser);
-        }
+        public void CancelEdit(User cancelUser) => UserGrid.CancelEditRow(cancelUser);
 
-        public void OnEditUser()
+        public void OnEditUser(User updatedUser)
         {
-            //Context.Save usw
+            try
+            {
+                IsLoading = true;
+                using var ctx = dbContextFactory.CreateDbContext();
+                var dbUser = ctx.Users.FirstOrDefault(u => u.Id == updatedUser.Id);
+                dbUser.Lastname = updatedUser.Lastname;
+                dbUser.Firstname = updatedUser.Firstname;
+                ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                IsLoading = false;
+            }
         }
 
         public void OnCreateUser()
